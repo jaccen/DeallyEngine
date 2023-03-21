d464830b2f78f49a64fbae5e53158a98f3a9b2bc
jaccen <jaccen2007@163.com>
2023-03-21 12:41:12 +0800

    Update RayTracing.m

---
+++ RayTracing/RayTracing.m
@@ -0,0 +1,16 @@
+1、什么是RayTracing
+
+
+2、开源框架
+   
+    1）Raytracing.Github.Io  https://github.com/RayTracing/raytracing.github.io     Html    Other
+    
+    2）Tinyraytracer  https://github.com/ssloy/tinyraytracer                        C++ 
+    
+    3）WickedEngine   https://github.com/turanszkij/WickedEngine                     C++     MIT
+     
+    4）DiligentEngine   https://github.com/DiligentGraphics/DiligentEngine           c++      Apache license 2.0
+    
+    5）Tinykaboom        https://github.com/ssloy/tinykaboom                          C++
+    
+    6）
